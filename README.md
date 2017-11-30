c4403cca635ce1d0478bcb98f80df5cacc616436
Jake Webb <jwebb@axiomadmin.com>
2017-11-30 14:33:53 -0600

    setup for viewing static index.html file

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 Steps to deploy:
 
+(if you have Node.js installed on local machine)
 1. npm install
 2. npm start
 3. React app served by node on localost:5000
+
+(static html page)
+1. find index.html in client/index.html
+2. load into any browser to view
--- client/components/layout/header.js
+++ client/components/layout/header.js
@@ -8,7 +8,7 @@ class Header extends React.Component {
             <div className='header'>
                 <a href="https://www.fourohseven.com" target="_blank">
                     <div className="logo-full-cover left"></div>
-                    <img className="logo" src="../../images/logo.png" />
+                    <img className="logo" src="./images/logo.png" />
                     <div className="logo-full-cover right"></div>
                 </a>
             </div>
